19dd51371849645e1556bfae74fefe59632bd240
Hilal Khan <106275724+hilalkhan1@users.noreply.github.com>
2024-03-25 02:18:40 +0500

    Time: 42 ms (43.51%), Space: 16.5 MB (27.03%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,19 @@
 class Solution:
     def fib(self, n: int) -> int:
+        f = [0] * (n+2)
+
+        f[0] = 0
+        f[1] = 1
+
+        for i in range(2, n+1):
+            f[i] = f[i - 1] + f[i - 2]
+
+        return f[n]
+
+
+
+
+
         if n <= 1:
             return n
         return self.fib(n-1) + self.fib(n-2)
