6b95f35957239db1eb44ac563d812149d8740b18
Hilal Khan <106275724+hilalkhan1@users.noreply.github.com>
2024-04-29 01:21:24 +0500

    Time: 29 ms (89.76%), Space: 16.7 MB (23.96%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,28 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
-        if n == 1:
-            return x
-        
-        y = x
-        if n > 500:
-            n = 500 / 4
-            for i in range(int(n)+1):
-                y *= x
-            return y**4
-            
 
+        def my_func(x, n):
+            if n == 0: 
+                return 1
+            result = my_func(x*x, n//2)
+            return result * x if n&1 else result
+        return my_func(x, n) if n>0 else 1 / my_func(x, -n)
         
         
 
-
-        if n < 0 :
-            n = -1 * n
-            for i in range(n-1):
-                y *=x
-            return 1 / y
-        for i in range(n-1):
-            y *= x
-        return y
         
