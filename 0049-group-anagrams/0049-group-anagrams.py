4fb02b76b19d0b786c1a83d2417182f45e64dc45
Hilal Khan <106275724+hilalkhan1@users.noreply.github.com>
2024-04-28 13:22:31 +0500

    Time: 84 ms (81.54%), Space: 20.3 MB (52.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,5 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if len(strs) == 0 or len(strs) == 1:
-            return [strs]
 
         d = defaultdict(list)
         for word in strs:
