0d7bcab14099817ea6f2d6be86a1520ccc93b6eb
Hilal Khan <106275724+hilalkhan1@users.noreply.github.com>
2024-05-04 16:50:06 +0500

    Time: 78 ms (42.61%), Space: 18 MB (9.72%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,32 +1,10 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        # n = len(nums)
-        # output = []
-
-        # if n == 0:
-        #     return [-1, -1]
-        # for i in range(n):
-        #     if nums[i] == target and len(output)<2:
-        #         output.append(i)
-        #         continue
-        #     if nums[i] == target and len(output) >= 2:
-        #         output.pop()
-        #         output.append(i)
-        # if len(output) == 0:
-        #     return [-1, -1]
-        # if len(output) == 1:
-        #     output.append(output[0])
-        #     return output
-
-        # return output
-
         n = len(nums)
         left, right = 0, n-1
         start = -1
-
         while (left <= right):
             mid = (left + right) // 2
-
             if target <= nums[mid]:
                 if target == nums[mid]:
                     start = mid
@@ -38,7 +16,6 @@ class Solution:
 
         while left <= right:
             mid = (left + right)//2
-
             if target >= nums[mid]:
                 if target == nums[mid]:
                     end = mid
@@ -47,7 +24,3 @@ class Solution:
                 right = mid - 1
 
         return [start, end] 
-
-
-
-        
